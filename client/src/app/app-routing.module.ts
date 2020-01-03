1d3e5ba3db9edfad2fd7a448d02d09c54b8cc9fa
trillionsun <aspensport072@gmail.vom>
2020-01-03 16:52:56 +1100

    Basic routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    component: HomeComponent,
+  },
+  { path: 'Dashboard',
+    component: DashboardComponent,
+  },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
